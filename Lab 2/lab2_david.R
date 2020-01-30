19b37f80103a0185b6d91f80941c1942d3470a86
David Hrabovszki <hrabovszkidavid@gmail.com>
2020-01-30 10:07:13 +0100

    ds

--- Lab 2/lab2_david.R
+++ Lab 2/lab2_david.R
@@ -1,6 +1,16 @@
 # Question 1: Optimizing a model parameter
+# R version
+RNGversion('3.5.1')
+
 
 ### 1.
+data <- read.csv2("mortality_rate.csv",sep=";")
+
+n=dim(data)[1]
+set.seed(123456)
+id=sample(1:n,floor(n*0.5))
+train=data[id,]
+test=data[-id,]
 
 ### 2.
 
@@ -16,6 +26,7 @@
 # Question 2: Maximizing likelihood
 
 ### 1.
+load("D:/LiU/Semester 2/Period 1/Computational Statistics/Computational-Statistics/Lab 2/data.RData")
 
 ### 2.
 
