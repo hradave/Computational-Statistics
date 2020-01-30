0b9caa6c15220eb08bcef0db28a456c1836aea85
David Hrabovszki <hrabovszkidavid@gmail.com>
2020-01-30 09:52:29 +0100

    hola

---
+++ Lab 2/lab2_david.R
@@ -0,0 +1,25 @@
+# Question 1: Optimizing a model parameter
+
+### 1.
+
+### 2.
+
+### 3.
+
+### 4.
+
+### 5.
+
+### 6.
+
+
+# Question 2: Maximizing likelihood
+
+### 1.
+
+### 2.
+
+### 3.
+
+### 4.
+
