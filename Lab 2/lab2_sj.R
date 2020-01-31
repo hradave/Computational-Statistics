f98708bae7f36504635602080329a7d455a096b6
sofficial.jorgensen@gmail.com <sofficial.jorgensen@gmail.com>
2020-01-31 09:44:39 +0100

    added Question 1

--- Lab 2/lab2_sj.R
+++ Lab 2/lab2_sj.R
@@ -7,6 +7,7 @@ RNGversion('3.5.1')
 
 # Packages
 library(tidyverse)
+library(ggplot2)
 
 #--------------------------------------------------------------------
 # QUESTION 1 - Optimizing a model parameter
@@ -15,6 +16,7 @@ library(tidyverse)
 # 1.1 
 # Import the mortality data set
 data <- read.csv2("mortality_rate.csv")
+
 
 # Add a new variable LMR
 data <- data %>%
@@ -27,9 +29,70 @@ id = sample(1:n, floor(n * 0.5))
 train = data[id ,]
 test = data[-id ,]
 
-# 1.2 
+# 1.2
 
-#
+# MSE function
+myMSE <- function(lambda, pars) {
+  X <- pars$X
+  Y <- pars$Y
+  Xtest <- pars$Xtest
+  Ytest <- pars$Ytest
+  model <- loess(Y ~ X, enp.target = lambda)
+  fYpred <- predict(model, newdata = Xtest)
+  MSE <- sum((Ytest - fYpred)^2) / length(Ytest)
+  return(MSE)
+}
+
+# 1.3
+
+
+pars <- list(X = as.matrix(train$Day),
+             Y = as.matrix(train$LMR),
+             Xtest = as.matrix(test$Day),
+             Ytest = as.matrix(test$LMR))
+
+# Lambda values [0.1,40]
+lambda <- seq(from = 0.1, to = 40, by = 0.1)
+
+# Estimate the predictive MSE values
+MSE <- sapply(lambda, myMSE, pars)
+
+
+# 1.4
+# Plot lambda vs MSE
+df_plot <- data.frame(lambda, MSE)
+ggplot(df_plot, aes(x = lambda, y = MSE)) +
+  geom_point(col = "darkblue") + 
+  xlab(expression(lambda)) + 
+  geom_vline(xintercept = 11.7)  # optimal lambda
+
+
+lambda_opt <- cbind(lambda, MSE)[which(MSE == min(MSE))]
+which(MSE == min(MSE))[1] 
+#MSE evaluations required to find optimal is 117
+
+### 5.
+
+MSE_min <- optimize(myMSE, interval = c(0.1, 40), tol = 0.01, pars = pars) #lambda = 10.69; MSE = 0.1321
+#The function didn't manage to find the optimal MSE value.
+# If we have optimized over a narrower interval of values, then the algorithm would be able to find the 
+# optimal value of MSE. But since the selected interval is a bad guess of where the optimal lamda is,
+# it does not find it. The function optimize() is for one dimensional optimazation and it seraches the given interval
+# for the optimum (min or max) w.r.t. its first argument. 
+
+#################### HOW MANY TIME DID OPTIMIZE() CALL THE FUNCTION MYMSE() ##################################
+
+### 6.
+
+# Now we use the function optim which in contrast to the optimize function, is for general purpose optimazation,
+# with the setting BFGS as the chosen method. The optim function does not find the optimal value when the stating point
+# lambda = 35. This is a pretty bad guess if we look at the plot. However if we pick a better guess as the starting
+# value, then it performs better. 
+
+MSE_min2 <- optim(method = "BFGS", par = 35, fn = myMSE, pars = pars)
+MSE_min2$counts[1] 
+
+# I requres one function evaluation to find 
 
 
 #--------------------------------------------------------------------
