a8c8cfb004e98e0562ebaf5cde27c3d769513de6
David Hrabovszki <hrabovszkidavid@gmail.com>
2020-02-26 12:18:03 +0100

    asdasd

--- Lab 5/david.R
+++ Lab 5/david.R
@@ -1,6 +1,7 @@
 ############### QUESTION 1 #################
 
 # 1
+library(boot)
 
 data <- read.csv2('lottery.csv')
 plot(data$Day_of_year,data$Draft_No) #looks random
@@ -14,6 +15,27 @@ points(Y_hat)
 
 # 3
 
+loess <- loess(Draft_No ~ Day_of_year,data = data)
+Y_hat <- predict(loess, data)
+Xb <- strtoi(names(which(Y_hat == max(Y_hat))))
+Xa <- strtoi(names(which(Y_hat == min(Y_hat))))
+
+
+stat <- function(data, vn) {
+  data_bs <- data[vn,]
+  loess <- loess(Draft_No ~ Day_of_year,data = data_bs)
+  Y_hat <- predict(loess, data_bs)
+  
+  Xb <- which.max(unname(Y_hat))
+  Xa <- which.min(unname(Y_hat))
+  
+  t_stat <- (unname(Y_hat[Xb]) - unname(Y_hat[Xa])) / (Xb - Xa)
+  
+  return(t_stat)
+  
+}
+
+res <- boot(data, stat, R = 2000)
 
 
 # 4
